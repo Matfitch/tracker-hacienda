a2021481cc080d34287712d945d36b98995ca421
Matfitch <mauriciotabango092@gmail.com>
2026-08-05 21:18:03 -0500

    Iconos trasparentes

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-edf91e0a'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.jqs6v2hqve"
+    "revision": "0.7a0a8c6e65k"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
